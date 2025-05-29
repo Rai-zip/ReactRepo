692d17984b30800f1e6bad9db9045c7a27b5d357
Anugrah Rai <118794485+Rai-zip@users.noreply.github.com>
2025-05-29 14:28:06 -0400

    Using Routing for the first time

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,22 @@ import './App.css'
 import Favorites from './pages/Favorites';
 import Home from './pages/Home'
 import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar';
 
 function App() {
   console.log("App loaded")
   return (
-    <main className='main-content'>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/favorites' element={<Favorites />}></Route>
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/favorites' element={<Favorites />}></Route>
+        </Routes>
+      </main>
+    </div>
   ); 
 }
 
+export default App;
 
-export default App
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+function NavBar(){
+
+    return <nav className="nav-bar">
+                <div className="navbar-brand">
+                    <Link to="/">Movie App </Link>
+                </div>
+                <div className="navbar-links">
+                    <Link to="/" className="nav-link"> Home </Link>
+                    <Link to="/favorites" className="nav-link"> Fav </Link>
+                </div>
+            </nav>
+}
+
+export default NavBar
