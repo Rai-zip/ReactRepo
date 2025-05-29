78d7642aedef1c60de8f5946a976726b672170c1
Anugrah Rai <118794485+Rai-zip@users.noreply.github.com>
2025-05-29 14:09:05 -0400

    Added routing into the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css'
+import Favorites from './pages/Favorites';
 import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   console.log("App loaded")
-  return <Home />
+  return (
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/favorites' element={<Favorites />}></Route>
+      </Routes>
+    </main>
+  ); 
 }
+
 
 export default App
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorites() {
+    return <div className="favorites-empty">
+        <h2>No Favorite movies yet!</h2>
+        <p> Start Adding Movies and they will appear here!</p>
+    </div>
+}
+
+export default Favorites
