d22252fd535467ba5575399fc4b27cd58ca3d782
Anugrah Rai <118794485+Rai-zip@users.noreply.github.com>
2025-05-29 00:04:13 -0400

    Introduced to the concept of fragments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,14 @@ import './App.css'
 function App() {
 
   return (
-    <div> 
-      <p>Hello world</p> 
-    </div>
+    <>
+      <div> 
+        <p>Hello world</p> 
+      </div>
+      <div> 
+        <p>Hello world</p> 
+      </div>
+    </>
   )
 }
 
