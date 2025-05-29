fdd1d8923f8f0e25dd2b0f8521dcb1d649f352e3
Anugrah Rai <118794485+Rai-zip@users.noreply.github.com>
2025-05-29 13:58:15 -0400

    Search function implemented

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import MovieCard from "../components/MovieCard"
+import { useState } from "react";
 
 function Home(){
+    const [searchQuery, setSearchQuery] = useState(""); 
+
     const movies = [
             {id: 1, title: "Anugrah", release_date: "2020"}, 
             {id: 2, title: "Hello", release_date: "2020"}, 
@@ -8,12 +11,32 @@ function Home(){
             {id: 4, title: "Star Wars", release_date: "2020"}, 
     ]
 
+    const handleSearch = (a) => {
+        a.preventDefault()
+        alert(searchQuery);
+        setSearchQuery("Sent!")
+    }
+
     return (
         <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input
+                type="text" 
+                placeholder="Serach for movies..." 
+                className="search-input"
+                value={searchQuery} 
+                onChange={(a) => setSearchQuery(a.target.value)}
+                />
+                <button type="submit" className="search-btn" onClick={handleSearch}> Search </button>
+            </form>
+
             <div className="movies-grid">
-                {movies.map((movie) => (
-                    <MovieCard movie={movie} key={movie.id}/>
-                ))}
+                {movies.map(
+                    (movie) => 
+                        movie.title.toLowerCase().startsWith(searchQuery) &&
+                        (<MovieCard movie={movie} key={movie.id}/>
+                    )
+                )}
             </div>`
         </div>
     );
